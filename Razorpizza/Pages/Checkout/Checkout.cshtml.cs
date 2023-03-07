2805ac5d1a9618cacd23b0379e77ad539cd56959
Fabian Kirchmann <Fabian_Kirchmann@users.noreply.github.com>
2023-03-07 18:38:26 +0100

    checkout

--- Razorpizza/Pages/Checkout/Checkout.cshtml.cs
+++ Razorpizza/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Razorpizza.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitel { get; set; }
         public void OnGet()
         {
+            if (string.IsNullOrEmpty(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if(string.IsNullOrEmpty(ImageTitel)) 
+            {
+                ImageTitel = "Create";
+            }
+
         }
     }
 }
